fcdaeee641f7181007646a8e31ab959b1343400f
David Kurilla <Kurilla.David@student.greenriver.edu>
2024-01-04 18:04:11 -0800

    Added a method to print a header.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,19 @@
 public class Main {
+
     public static void main(String[] args) {
+
+        printHeader();
         printMessage("Hello, world!");
+
+        printHeader();
         printMessage("By: David");
     }
 
     private static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    private static void printHeader() {
+        System.out.println("******************************");
     }
 }
