30aa0865d9a03dd9e04c41b02214776e2e1e7990
David Kurilla <Kurilla.David@student.greenriver.edu>
2024-01-04 18:09:22 -0800

    Re-added the printMessage() method.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        System.out.println("By: David");
+        printMessage("Hello, world!");
+        printMessage("By: David");
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(message);
     }
 }
